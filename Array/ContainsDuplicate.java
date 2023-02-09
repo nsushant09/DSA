de20d542228aa1967f1c1c0f3eb743d9ca882b23
Sushant Neupane <nsushant09@gmail.com>
2023-02-09 21:38:56 +0545

    LC DS1 1

--- Array/ContainsDuplicate.java
+++ Array/ContainsDuplicate.java
@@ -1,2 +1,19 @@
-package Array;public class ContainsDuplicate {
+package Array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+
+    }
+
+    static public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1])
+                return true;
+        }
+        return false;
+    }
 }
