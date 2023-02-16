4837d2ee8b1d2e1b517e6e7a3492c0059a3dec95
Sushant Neupane <nsushant09@gmail.com>
2023-02-16 16:43:50 +0545

    LC SORT 2418

---
+++ BubbleSort/SortPeople.java
@@ -0,0 +1,32 @@
+package BubbleSort;
+
+import java.util.Arrays;
+
+public class SortPeople {
+    public static void main(String[] args) {
+        int[] heights = {155, 185, 150};
+        String[] names = {"Alice", "Bob", "Bob"};
+        System.out.println(Arrays.toString(sortPeople(names, heights)));
+    }
+
+    static public String[] sortPeople(String[] names, int[] heights) {
+
+        for (int i = 0; i < heights.length - 1; i++) {
+            for (int j = i + 1; j < heights.length; j++) {
+
+                if (heights[i] < heights[j]) {
+                    int temp = heights[i];
+                    heights[i] = heights[j];
+                    heights[j] = temp;
+
+                    String tempName = names[i];
+                    names[i] = names[j];
+                    names[j] = tempName;
+                }
+
+            }
+        }
+
+        return names;
+    }
+}
