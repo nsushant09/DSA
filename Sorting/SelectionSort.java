a68327baaa7941d8cfa8fe1628db310a89a28649
Sushant Neupane <nsushant09@gmail.com>
2023-02-17 16:39:36 +0545

    Selection Sort

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,2 +1,31 @@
-package Sorting;public class SelectionSort {
+package Sorting;
+
+import java.util.Arrays;
+
+//find max element in given array
+//push it to the end
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 1, 4, 3};
+        System.out.println(Arrays.toString(selectionSort(arr)));
+    }
+
+    static public int getMaxElementIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i < end; i++) {
+            if (arr[i] > arr[max])
+                max = i;
+        }
+        return max;
+    }
+
+    static public int[] selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int maxIndex = getMaxElementIndex(arr, 0, arr.length - i);
+            int temp = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = arr[maxIndex];
+            arr[maxIndex] = temp;
+        }
+        return arr;
+    }
 }
